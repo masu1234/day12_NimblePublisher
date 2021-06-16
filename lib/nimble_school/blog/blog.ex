818d34265e34da78346537ad3f69dd07661da37e
endoakak <akendo2020@gmail.com>
2021-06-16 13:00:30 +0900

    endo 1st

---
+++ lib/nimble_school/blog/blog.ex
@@ -0,0 +1,12 @@
+defmodule NimbleSchool.Blog do
+  alias NimbleSchool.Blog.Post
+  use NimblePublisher,
+    build: Post,
+    from: Application.app_dir(:nimble_school, "priv/posts/**/*.md"),
+    as: :posts,
+    highlighters: [:makeup_elixir, :makeup_erlang]
+  @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
+  @tags @posts |> Enum.flat_map(& &1.tags) |> Enum.uniq() |> Enum.sort()
+  def all_posts, do: @posts
+  def all_tags, do: @tags
+end
---
+++ priv/posts/2021/06-16-exciting-news.md
@@ -0,0 +1,10 @@
+%{
+    title: "Exciting News",
+    author: "masu",
+    tags: ~w(exciting news),
+    description: "Our exciting news is here"
+}
+
+---
+
+Yes, this is **the post** you've been waiting for.
